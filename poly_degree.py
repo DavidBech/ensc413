296fd10057604f71af4a1cb436cd9103ca068e45
David <dbechert@sfu.ca>
2022-01-16 13:56:20 -0800

    added both train and validation error scores

--- poly_degree.py
+++ poly_degree.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
 
 # read the dataframe df from the csv file
 df = pd.read_csv("polydata.csv")
@@ -12,26 +13,32 @@ y = df.loc[:,'y'].values
 # add your code below
 
 maxDeg = 10
+trainingSplit = 0.4
+randomState = 32
+xTrain, xValidate, yTrain, yValidate = train_test_split(x, y, test_size=trainingSplit, random_state=randomState)
 
 #degree k goes from 0 to 10
 polyList = [None] * (maxDeg + 1)
 plotList = [None] * (maxDeg + 1)
-polyError = [None] * (maxDeg + 1)
+trainingError = [None] * (maxDeg + 1)
+validateError = [None] * (maxDeg + 1)
 for k in range(maxDeg + 1):
     # fit the values for a degree k poly
-    coef = np.polyfit(x, y, k) 
+    coef = np.polyfit(xTrain, yTrain, k) 
 
     # store the polynomial
     polyList[k] = np.poly1d(coef)
 
     # calculate error in polynomial
-    X = np.vander(x, k + 1)
+    XTrain = np.vander(xTrain, k + 1)
+    XVal = np.vander(xValidate, k + 1)
     # E = || y - Xa ||^2
-    polyError[k] = np.linalg.norm((y - np.matmul(X, coef)))
-    print(f"Error In poly {k: <2}: {polyError[k]}")
+    trainingError[k] = np.linalg.norm((yTrain - np.matmul(XTrain, coef)))
+    validateError[k] = np.linalg.norm((yValidate - np.matmul(XVal, coef)))
+    print(f"Error In poly {k: <2}: Train:{trainingError[k]: <20} Validate:{validateError[k]}")
 
     # plot polynomial if error is sufficiently small (value from successive runs)
-    if polyError[k] < 139:
+    if validateError[k] < 100:
         plotList[k] = plt.plot(x, polyList[k](x), label = f"poly-degree:{k}")
 
 
