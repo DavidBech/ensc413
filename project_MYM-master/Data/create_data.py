741d4e76304cc2b98d03ff2fb88c27cad3a4bc20
David <dbechert@sfu.ca>
2022-03-27 12:11:00 -0700

    made create data into command line script

--- project_MYM-master/Data/create_data.py
+++ project_MYM-master/Data/create_data.py
@@ -183,97 +183,99 @@ def writeSquaresToFile(img, midPoints, cornerPoints, imgName, json, folder="./ra
             croppedImg = img [startY: endY, startX: endX]
             cv2.imwrite(folder + classificationFolder + "/" + imgName + "_" + locationString + ".jpeg", croppedImg)
 
-# Create a list of image file names
-img_filename_list = []
-folder_name = './full_data/imag/r*'
-for path_name in glob.glob(folder_name):
-    # file_name = re.search("[\w-]+\.\w+", path_name) (use if in same folder)
-    img_filename_list.append(path_name)  # file_name.group()
 
-#print(img_filename_list)
-# Create and save cropped images from original images to the data folder
-#img_count = 20000
-print_number = 0
-for file_name in img_filename_list:
-    print(file_name)
-    imageName = file_name.split("\\")[-1].split(".")[0]
-    # get grayscale image
-    img, gray_blur = read_img(file_name)
+if __name__ == "__main__":
+    # Create a list of image file names
+    img_filename_list = []
+    sub_folder = "real"
+    folder_name = './full_data/' + sub_folder + '/imag/*'
+    for path_name in glob.glob(folder_name):
+        # file_name = re.search("[\w-]+\.\w+", path_name) (use if in same folder)
+        img_filename_list.append(path_name)  # file_name.group()
 
-    # get edges in image
-    edges = canny_edge(gray_blur, 1)
+    # Create and save cropped images from original images to the data folder
+    #img_count = 20000
+    print_number = 0
+    for file_name in img_filename_list:
+        print(file_name)
+        imageName = file_name.split("\\")[-1].split(".")[0]
+        # get grayscale image
+        img, gray_blur = read_img(file_name)
 
-    # get hough lines from edges
-    raw, lines = hough_line(edges)
+        # get edges in image
+        edges = canny_edge(gray_blur, 1)
 
-    # display lines on image
-    #img0 = img.copy()
-    #for i in range(0, len(raw)):
-    #    rho = raw[i][0][0]
-    #    theta = raw[i][0][1]
-    #    a = math.cos(theta)
-    #    b = math.sin(theta)
-    #    x0 = a*rho
-    #    y0 = b*rho
-    #    pt1  = (int(x0 + 1000*(-b)), int(y0 + 1000*a))
-    #    pt2  = (int(x0 - 1000*(-b)), int(y0 - 1000*a))
-    #    cv2.line(img0, pt1, pt2, (0,0,255), 3, cv2.LINE_AA)
-    #cv2.imshow("lines", img0)
+        # get hough lines from edges
+        raw, lines = hough_line(edges)
 
-    # separate horizontal and vertical lines from hough lines
-    h_lines, v_lines = h_v_lines(lines)
-    # get intersection points of horizontal and vertical lines
-    intersection_points = line_intersections(h_lines, v_lines)
-    # make points close together into one point
-    points = cluster_points(intersection_points)
+        # display lines on image
+        #img0 = img.copy()
+        #for i in range(0, len(raw)):
+        #    rho = raw[i][0][0]
+        #    theta = raw[i][0][1]
+        #    a = math.cos(theta)
+        #    b = math.sin(theta)
+        #    x0 = a*rho
+        #    y0 = b*rho
+        #    pt1  = (int(x0 + 1000*(-b)), int(y0 + 1000*a))
+        #    pt2  = (int(x0 - 1000*(-b)), int(y0 - 1000*a))
+        #    cv2.line(img0, pt1, pt2, (0,0,255), 3, cv2.LINE_AA)
+        #cv2.imshow("lines", img0)
 
-    #img2 = img.copy()
-    #for i, point in enumerate(points):
-    #    #img2 = cv2.circle(img2, (int(point[0]), int(point[1])), radius = 1, color=(255,0,0), thickness=-1)
-    #    cv2.putText(img2, str(i), (int(point[0]), int(point[1])), cv2.FONT_HERSHEY_SIMPLEX, 1, (209,80,0,255), 3)
-    #cv2.imshow("point", img2)
+        # separate horizontal and vertical lines from hough lines
+        h_lines, v_lines = h_v_lines(lines)
+        # get intersection points of horizontal and vertical lines
+        intersection_points = line_intersections(h_lines, v_lines)
+        # make points close together into one point
+        points = cluster_points(intersection_points)
 
-    if len(points) < 81:
-        print ("not enough intersection points")
-        assert False
-    elif len(points) > 81:
-        #TODO this could become a problem later
-        pass
+        #img2 = img.copy()
+        #for i, point in enumerate(points):
+        #    #img2 = cv2.circle(img2, (int(point[0]), int(point[1])), radius = 1, color=(255,0,0), thickness=-1)
+        #    cv2.putText(img2, str(i), (int(point[0]), int(point[1])), cv2.FONT_HERSHEY_SIMPLEX, 1, (209,80,0,255), 3)
+        #cv2.imshow("point", img2)
 
-    rows = [[]]*9
-    for i in range(9):
-        rows[i] = points[9*i:9*i+9]
-        rows[i] = sorted(rows[i], key = lambda k: [k[0], k[1]])
+        if len(points) < 81:
+            print ("not enough intersection points")
+            assert False
+        elif len(points) > 81:
+            #TODO this could become a problem later
+            pass
 
-    #img3 = img.copy()
-    #for r, row in enumerate(rows):
-    #    for i, point in enumerate(row):
-    #        cv2.putText(img3, str(r) + str(i), (int(point[0]), int(point[1])), cv2.FONT_HERSHEY_SIMPLEX, 1, (209,80,0,255), 3)
-    #cv2.imshow("row point", img3)
+        rows = [[]]*9
+        for i in range(9):
+            rows[i] = points[9*i:9*i+9]
+            rows[i] = sorted(rows[i], key = lambda k: [k[0], k[1]])
 
-    tileMidPoints = []
-    for i in range(8):
-        tileMidPoints.append([])
-        for j in range(8):
-            # x coord
-            tileMidPoints[i].append((0.25*(rows[i][j][0] + rows[i+1][j][0] + rows[i][j+1][0] + rows[i+1][j+1][0]), 0.25*(rows[i][j][1] + rows[i+1][j][1] + rows[i][j+1][1] + rows[i+1][j+1][1])))
+        #img3 = img.copy()
+        #for r, row in enumerate(rows):
+        #    for i, point in enumerate(row):
+        #        cv2.putText(img3, str(r) + str(i), (int(point[0]), int(point[1])), cv2.FONT_HERSHEY_SIMPLEX, 1, (209,80,0,255), 3)
+        #cv2.imshow("row point", img3)
 
-    #img4 = img.copy()
-    #for r, row in enumerate(tileMidPoints):
-    #    for i, point in enumerate(row):
-    #        img4 = cv2.circle(img4, (int(point[0]), int(point[1])), radius = 1, color=(255,0,0), thickness=-1)
-    #        cv2.putText(img4, string.ascii_lowercase[r] + str(i+1), (int(point[0]), int(point[1])), cv2.FONT_HERSHEY_SIMPLEX, 1, (209,80,0,255), 3)
-    #cv2.imshow("mid point", img4)
+        tileMidPoints = []
+        for i in range(8):
+            tileMidPoints.append([])
+            for j in range(8):
+                # x coord
+                tileMidPoints[i].append((0.25*(rows[i][j][0] + rows[i+1][j][0] + rows[i][j+1][0] + rows[i+1][j+1][0]), 0.25*(rows[i][j][1] + rows[i+1][j][1] + rows[i][j+1][1] + rows[i+1][j+1][1])))
 
-    correspondingJson = "./full_data/json/" + imageName + ".json"
-    jsonFile = open(correspondingJson, "r")
-    data = json.load(jsonFile)
-    writeSquaresToFile(img, tileMidPoints, rows, imageName, data)
-    jsonFile.close()
-    #print('points: ' + str(np.shape(points)))
-    #img_count = write_crop_images(img, points, img_count)
-    #print('img_count: ' + str(img_count))
-    #print('PRINTED')
-    #print_number += 1
-    cv2.waitKey(0)
-#print(print_number)
+        #img4 = img.copy()
+        #for r, row in enumerate(tileMidPoints):
+        #    for i, point in enumerate(row):
+        #        img4 = cv2.circle(img4, (int(point[0]), int(point[1])), radius = 1, color=(255,0,0), thickness=-1)
+        #        cv2.putText(img4, string.ascii_lowercase[r] + str(i+1), (int(point[0]), int(point[1])), cv2.FONT_HERSHEY_SIMPLEX, 1, (209,80,0,255), 3)
+        #cv2.imshow("mid point", img4)
+
+        correspondingJson = "./full_data" + sub_folder + "json/" + imageName + ".json"
+        jsonFile = open(correspondingJson, "r")
+        data = json.load(jsonFile)
+        writeSquaresToFile(img, tileMidPoints, rows, imageName, data)
+        jsonFile.close()
+        #print('points: ' + str(np.shape(points)))
+        #img_count = write_crop_images(img, points, img_count)
+        #print('img_count: ' + str(img_count))
+        #print('PRINTED')
+        #print_number += 1
+        cv2.waitKey(0)
+    #print(print_number)
