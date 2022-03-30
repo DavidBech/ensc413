d8fffc5e7d970929610d173dfae35535d34ca8ec
David <dbechert@sfu.ca>
2022-03-29 21:00:29 -0700

    added test to setup script

--- Data/setup_data.py
+++ Data/setup_data.py
@@ -4,7 +4,7 @@ import create_data_common
 import random 
 import glob
 
-# Data to include as well as the quantity of data
+# Data to include in training
 includeRealTrain = True
 realTrainPercent = 1.00
 
@@ -14,6 +14,16 @@ unityTrainPercent = 1.00
 includeBlenderTrain = False
 blenderTrainPercent = 1.00
 
+# Data to include in testing
+includeRealTest = True
+realTestPercent = 1.00
+
+includeUnityTest = False
+unityTestPercent = 1.00
+
+includeBlenderTest = False
+blenderTestPercent = 1.00
+
 # location of test and train out paths
 testOutPath = "test"
 trainOutPath = "train"
@@ -21,6 +31,12 @@ trainOutPath = "train"
 def copyFiles(fromDir, toDir, namePreppend, copyPercent):
     coppiedCount = 0
     fileCount = 0
+    totalFiles = 0
+
+    for category in os.listdir(fromDir):
+        fromPath = fromDir + "/" + category
+        totalFiles += len(os.listdir(fromPath))
+
     for category in os.listdir(fromDir):
         fromPath = fromDir + "/" + category
         toPath = toDir + "/" + category + "/"
@@ -30,7 +46,10 @@ def copyFiles(fromDir, toDir, namePreppend, copyPercent):
             if random.random() < copyPercent:
                 shutil.copy(fromPath + "/" + file, toPath + "/" + namePreppend + file)
                 coppiedCount += 1
-    print(f"Finished Copying: {coppiedCount} out of {fileCount}")
+            if (fileCount % 1000) == 0:
+                print(f"Processed {fileCount} out of {totalFiles}")
+        
+    print(f"Finished -- copied: {coppiedCount} out of {totalFiles}")
 
 if __name__ == "__main__":
     create_data_common.setupDirs(testOutPath, trainOutPath)
@@ -46,4 +65,16 @@ if __name__ == "__main__":
     if includeBlenderTrain:
         print("Starting Copying Blender Training Files")
         copyFiles("./train_blender", "./train", "blender", blenderTrainPercent)
+
+    if includeRealTest:
+        print("Starting Copying Real Training Files")
+        copyFiles("./test_real", "./test", "real", realTestPercent)
+
+    if includeUnityTest:
+        print("Starting Copying Unity Training Files")
+        copyFiles("./test_unity", "./test", "unity", unityTestPercent)
+    
+    if includeBlenderTest:
+        print("Starting Copying Blender Training Files")
+        copyFiles("./test_blender", "./test", "blender", blenderTestPercent)
 
