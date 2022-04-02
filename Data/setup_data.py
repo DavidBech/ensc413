fea4b885fe109899791c5ca46d63f32addc2e42c
David <dbechert@sfu.ca>
2022-04-01 22:26:41 -0700

    output setup data results to json file

--- Data/setup_data.py
+++ Data/setup_data.py
@@ -3,10 +3,11 @@ import os
 import create_data_common
 import random 
 import glob
+import json
 
 # Data to include in training
 includeRealTrain = True
-realTrainPercent = 1.00
+realTrainPercent = 0.1
 
 includeUnityTrain = False
 unityTrainPercent = 1.00
@@ -16,7 +17,7 @@ blenderTrainPercent = 1.00
 
 # Data to include in testing
 includeRealTest = True
-realTestPercent = 1.00
+realTestPercent = 0.1
 
 includeUnityTest = False
 unityTestPercent = 1.00
@@ -32,6 +33,7 @@ def copyFiles(fromDir, toDir, namePreppend, copyPercent):
     coppiedCount = 0
     fileCount = 0
     totalFiles = 0
+    categoryCount = {}
 
     for category in os.listdir(fromDir):
         fromPath = fromDir + "/" + category
@@ -40,41 +42,92 @@ def copyFiles(fromDir, toDir, namePreppend, copyPercent):
     for category in os.listdir(fromDir):
         fromPath = fromDir + "/" + category
         toPath = toDir + "/" + category + "/"
-
+        categoryCount[category] = 0
         for file in os.listdir(fromPath):
             fileCount += 1
             if random.random() < copyPercent:
                 shutil.copy(fromPath + "/" + file, toPath + "/" + namePreppend + file)
                 coppiedCount += 1
+                categoryCount[category] += 1
             if (fileCount % 1000) == 0:
                 print(f"Processed {fileCount} out of {totalFiles}")
         
     print(f"Finished -- copied: {coppiedCount} out of {totalFiles}")
+    return categoryCount
 
 if __name__ == "__main__":
     create_data_common.setupDirs(testOutPath, trainOutPath)
+    realTrainFiles = None
+    unityTrainFiles = None
+    blenderTrainFiles = None
+    realTestFiles = None
+    unityTestFiles = None
+    blenderTestFiles = None
 
     if includeRealTrain:
         print("Starting Copying Real Training Files")
-        copyFiles("./train_real", "./train", "real", realTrainPercent)
+        realTrainFiles = copyFiles("./train_real", "./train", "real", realTrainPercent)
 
     if includeUnityTrain:
         print("Starting Copying Unity Training Files")
-        copyFiles("./train_unity", "./train", "unity", unityTrainPercent)
+        unityTrainFiles = copyFiles("./train_unity", "./train", "unity", unityTrainPercent)
     
     if includeBlenderTrain:
         print("Starting Copying Blender Training Files")
-        copyFiles("./train_blender", "./train", "blender", blenderTrainPercent)
+        blenderTrainFiles = copyFiles("./train_blender", "./train", "blender", blenderTrainPercent)
 
     if includeRealTest:
         print("Starting Copying Real Testing Files")
-        copyFiles("./test_real", "./test", "real", realTestPercent)
+        realTestFiles = copyFiles("./test_real", "./test", "real", realTestPercent)
 
     if includeUnityTest:
         print("Starting Copying Unity Testing Files")
-        copyFiles("./test_unity", "./test", "unity", unityTestPercent)
+        unityTestFiles = copyFiles("./test_unity", "./test", "unity", unityTestPercent)
     
     if includeBlenderTest:
         print("Starting Copying Blender Testing Files")
-        copyFiles("./test_blender", "./test", "blender", blenderTestPercent)
+        blenderTestFiles = copyFiles("./test_blender", "./test", "blender", blenderTestPercent)
+
+    jsonStr = f"""{{
+    "train" : {{
+        "real" : {{
+            "use": {"true" if includeRealTrain else "false"},
+            "percent": {realTrainPercent},
+            "files": "{realTrainFiles:}"
+        }},
+        "unity" : {{
+            "use": {"true" if includeUnityTrain else "false"},
+            "percent": {unityTrainPercent},
+            "files": "{unityTrainFiles}"
+
+        }},
+        "blender" : {{
+            "use": {"true" if includeBlenderTrain else "false"},
+            "percent": {blenderTrainPercent},
+            "files": "{blenderTrainFiles}"
+        }}
+    }},
+    "test" : {{
+        "real" : {{
+            "use": {"true" if includeRealTest else "false"},
+            "percent": {realTestPercent},
+            "files": "{realTestFiles}"
+        }},
+        "unity" : {{
+            "use": {"true" if includeUnityTest else "false"},
+            "percent": {unityTestPercent},
+            "files": "{unityTestFiles}"
+
+        }},
+        "blender" : {{
+            "use": {"true" if includeBlenderTest else "false"},
+            "percent": {blenderTestPercent},
+            "files": "{blenderTestFiles}"
+        }}   
+    }}  
+}}
+""" 
+    with open("./data_setup.json", "w") as f:
+        print(jsonStr, file=f)
+
 
